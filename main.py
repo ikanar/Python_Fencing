3b4732c3c096360b31ac51e036f0c16d1e653cdf
ikanar <ian.kanar@gmail.com>
2024-11-05 14:57:01 -0500

    Beginning of Project
    opens browser and navigates to the users fencingtracker.com page

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+
+
+if __name__ == "__main__":
+    name = input("Please enter fencer name: ")
+    driver = webdriver.Chrome()
+    driver.get("https://www.fencingtracker.com")
+    driver.find_element(By.XPATH, '//*[@class="navbar-toggler"]').click()
+    sleep(3)
+    searchbox = driver.find_element(By.XPATH, '//*[@id="searchTextbox"]').send_keys(name + "\n")
+    sleep(3)
+   
+
+    
