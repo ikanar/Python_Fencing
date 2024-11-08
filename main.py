75b668740bd7d09e7c4093830be124c924978b3c
ikanar <ian.kanar@gmail.com>
2024-11-08 14:13:08 -0500

    Update main.py
    -shortened sleep to 1 second
    - now prints all tournament names

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
 
 
 
@@ -11,15 +13,24 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://www.fencingtracker.com")
     driver.find_element(By.XPATH, '//*[@class="navbar-toggler"]').click()
-    sleep(3)
+    sleep(1)
     searchbox = driver.find_element(By.XPATH, '//*[@id="searchTextbox"]').send_keys(name + "\n")
-    sleep(3)
+    sleep(1)
     
     history_link = driver.find_element(By.LINK_TEXT,"History")
     history_link.click()
-    print(driver.current_url)
 
-    sleep(4)
+    elements = driver.find_elements(By.TAG_NAME,"h4")
+    sleep(1)
+
+    html=driver.page_source
+    soup = BeautifulSoup(html, features = "html.parser")
+
+    h4_elements = soup.find_all('h4')
+
+    for element in h4_elements:
+     print (element.text)
+
 
 
 
