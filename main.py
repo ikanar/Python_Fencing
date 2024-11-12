e38fcfcfeab1256b6d85a12c528ecfa447cd401e
ikanar <ian.kanar@gmail.com>
2024-11-11 23:06:09 -0500

    Update main.py
    now prints out all td elements

--- main.py
+++ main.py
@@ -31,6 +31,10 @@ if __name__ == "__main__":
     for element in h4_elements:
      print (element.text)
 
+    td_elements = soup.find_all('td')
+    for element in td_elements:
+       print(element.text)
+
 
 
 
